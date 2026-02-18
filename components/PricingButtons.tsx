c08801d10df48471715ab073b205fc76edbb6bcb
auto.grow.media077 <auto.grow.media077@gmail.com>
2026-02-18 08:12:40 +0000

    Added payment links

--- components/PricingButtons.tsx
+++ components/PricingButtons.tsx
@@ -6,17 +6,17 @@ export const PricingButtons: React.FC = () => {
     <div className="flex flex-col sm:flex-row gap-4">
       <PayUButton
         label="Buy Now for 399"
-        href="https://u.payu.in/BrUiQYLkFlAq"
+        href="https://payments.cashfree.com/forms/pinnaclepluspro"
       />
 
       <PayUButton
         label="Pay Now 999"
-        href="https://u.payu.in/HJh89xerzBHD"
+        href="https://payments.cashfree.com/forms/pinnaclepluspro"
       />
 
       <PayUButton
         label="Pay Now 1999"
-        href="https://u.payu.in/HJh89xerzBHD"
+        href="https://payments.cashfree.com/forms/pinnacleplusbusiness"
       />
     </div>
   );
